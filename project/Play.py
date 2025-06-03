6174948d5cb63854ea6a5c7b865ef6432d35809f
miriam40421 <miriam40421@gmail.com>
2025-06-03 19:29:05 +0300

    Deleted file(c)

--- project/Play.py
+++
@@ -1,9 +0,0 @@
-import random
-
-# פונקציית הגרלת המילה
-def random_word(num = int(input("enter number:"))):
-    file = open("./Words", 'r')
-    list_word = file.readlines()
-    random.shuffle(list_word)
-    word = list_word[num % list_word.__len__()]
-    return word
